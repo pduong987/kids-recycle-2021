3297b8bb396ef5ef8a0a311a1f9b37a817391759
Duong Pham <ptduong987@gmail.com>
2021-09-04 16:16:38 +1000

    Added more validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,12 @@ class Listing < ApplicationRecord
   belongs_to :buyer, class_name: "Profile", optional: true
   belongs_to :seller, class_name: "Profile"
   has_many_attached :pictures
+
+  # Add Validations
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: -1}
+  validates :status, presence: true
+  validates :location, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,4 +6,8 @@ class Profile < ApplicationRecord
   has_many :reviews_to_make, class_name: "Listing", foreign_key: "buyer_id"
   has_many :reviews_to_receive, class_name: "Listing", foreign_key: "seller_id"
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :user_name, presence: true, uniqueness: true 
+  
 end
